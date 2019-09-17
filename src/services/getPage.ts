ff45ae28d5c1e1495834f095a930e1ae8224e30a
Cris <xyxabcdiy@github.com>
2019-09-17 17:54:57 +0800

    fix: 修复接口数据处理错误

--- src/services/getPage.ts
+++ src/services/getPage.ts
@@ -2,11 +2,12 @@ import request from '../utils/request';
 import API from '../utils/request/api';
 
 export async function getPage(pageName: string) {
-    const templateConfig = await request({
+    const response = await request({
         url: API.template,
         params: {
             pageName
         }
     });
-    return templateConfig;
+    const pageData = JSON.parse(response.pageData);
+    return pageData;
 }
