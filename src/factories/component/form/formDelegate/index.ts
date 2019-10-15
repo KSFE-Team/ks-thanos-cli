dee76f657f6c326f53465c0db4f9e70d0ce119e5
Cris <xyxabcdiy@github.com>
2019-10-15 12:33:07 +0800

    format:

--- src/factories/component/form/formDelegate/index.ts
+++ src/factories/component/form/formDelegate/index.ts
@@ -8,11 +8,38 @@ export abstract class FormDelegate {
         this.form = form;
     }
 
+    /**
+     * 初始化页面的 decorator
+     */
     initPageDecorators?(): void;
+
+    /**
+     * 初始化页面的方法
+     */
     initPageMethods?(): void;
+
+    /**
+     * 初始页面的标题
+     */
     initPageTitle?(): void;
+
+    /**
+     * 初始页面的状态
+     */
     initPageState?(): void;
+
+    /**
+     * 初始effect
+     */
     abstract initEffects(): void;
+
+    /**
+     * 获取import
+     */
     abstract getImports(): Import[];
+
+    /**
+     * 生成代码
+     */
     abstract toCode(): string;
 }
