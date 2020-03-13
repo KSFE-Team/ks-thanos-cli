ffcd7116e9f0a21ca75df488fe8d1a62b4dd6084
caic <caichenyu@ksjgs.com>
2020-03-13 20:37:59 +0800

    feat: 修复本地路径引用错误问题

--- packages/ks-thanos-cli/src/index.ts
+++ packages/ks-thanos-cli/src/index.ts
@@ -1,8 +1,8 @@
-import 'module-alias/register';
+const moduleAlias = require('module-alias');
+moduleAlias.addAlias('Src', __dirname);
 import { runSync } from 'Src/commands/sync';
 import { runInit } from 'Src/commands/init';
 import { runUi } from 'Src/commands/ui';
-import 'module-alias/register';
 
 export {
     runSync,
