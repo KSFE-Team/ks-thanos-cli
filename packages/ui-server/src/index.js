fadb5c0b17734373644795369bbd3dca1a33efba
许垚翔 <xuyaoxiang@ksjgs.com>
2020-08-07 18:32:56 +0800

    fix: 支持ui-server本地调试

--- packages/ui-server/src/index.js
+++ packages/ui-server/src/index.js
@@ -74,7 +74,9 @@ app.use((context, next) => {
 //         // logger.error(err);
 //     });
 
-// startServer();
+if (process.env.LOCAL_DEBUG) {
+    startServer();
+}
 
 export function startServer(port = 3000) {
     server.listen(port, () => {
