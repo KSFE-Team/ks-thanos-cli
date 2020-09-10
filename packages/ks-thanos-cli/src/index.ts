2b2eb5fb7119a04440d55f4d22bc2e9364f87a68
wangmeng <wangmeng@ksjgs.com>
2020-09-10 15:55:16 +0800

    feat: 增加mergeNginx命令 拉取最新nginx配置同步到本地

--- packages/ks-thanos-cli/src/index.ts
+++ packages/ks-thanos-cli/src/index.ts
@@ -3,9 +3,11 @@ moduleAlias.addAlias('Src', __dirname);
 import { runSync } from 'Src/commands/sync';
 import { runInit } from 'Src/commands/init';
 import { runUi } from 'Src/commands/ui';
+import { runMergeNginx } from 'Src/commands/mergeNginx';
 
 export {
     runSync,
     runInit,
-    runUi
+    runUi,
+    runMergeNginx
 };
