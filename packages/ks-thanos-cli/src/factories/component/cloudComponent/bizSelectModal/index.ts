fb23e1c74b3877e0d217f3d081143e9b0c31c1f9
wangmeng <wangmeng@ksjgs.com>
2020-10-27 16:37:48 +0800

    feat: 提交修改

--- packages/ks-thanos-cli/src/factories/component/cloudComponent/bizSelectModal/index.ts
+++ packages/ks-thanos-cli/src/factories/component/cloudComponent/bizSelectModal/index.ts
@@ -1,5 +1,3 @@
-import Page from 'Src/factories/page';
-import { FormItemConfig } from '../../formItem';
 import { getPropStr } from 'Src/utils/getPropValue';
 import KMSCloudComponent from '../KMSCloudComponent';
 
@@ -8,30 +6,8 @@ import KMSCloudComponent from '../KMSCloudComponent';
  */
 export default class BizSelectModal extends KMSCloudComponent {
 
-    config: FormItemConfig
-
-    packageName: string
-
-    constructor(page: Page, config: FormItemConfig) {
-        super(page, config);
-        this.componentName = config.componentName;
-        this.packageName = `@ks/kms-${this.componentName.toLowerCase()}`;
-        this.config = config;
-    }
-
     getDecoratorConfigCode() {
         return '{}';
-    }
-
-    getImports() {
-        let imports = [
-            {
-                source: `Src/components/${this.packageName}`,
-                name: this.componentName,
-                defaultImport: this.config.default
-            }
-        ];
-        return imports;
     }
 
     toCode() {
