07ed62c2a486a6238381b97b267b7617120d2824
sunjiaojian <sunjiaojian@ksjgs.com>
2020-10-28 11:15:45 +0800

    feat: biztimingsetting

---
+++ packages/ks-thanos-cli/src/factories/component/cloudComponent/bizTimingSetting/index.ts
@@ -0,0 +1,22 @@
+import { getPropStr } from 'Src/utils/getPropValue';
+import KMSCloudComponent from '../KMSCloudComponent';
+
+/**
+ * bizTimingSetting组件
+ */
+export default class BizSelectTags extends KMSCloudComponent {
+
+    getDecoratorConfigCode() {
+        return '{}';
+    }
+
+    toCode() {
+        const propsCode = [];
+        for (let propKey in this.props) {
+            propsCode.push(getPropStr(propKey, this.props[propKey]));
+        }
+        return `<${this.componentName}
+        ${propsCode.join('\n')}
+    />`;
+    }
+}
