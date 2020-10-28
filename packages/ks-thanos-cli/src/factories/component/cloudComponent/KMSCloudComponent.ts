1910d78738be066a6b76f45e1dadab041fba21b5
wangmeng <wangmeng@ksjgs.com>
2020-10-28 18:22:27 +0800

    feat: 解析ast方法

--- packages/ks-thanos-cli/src/factories/component/cloudComponent/KMSCloudComponent.ts
+++ packages/ks-thanos-cli/src/factories/component/cloudComponent/KMSCloudComponent.ts
@@ -1,10 +1,9 @@
 import Page from 'Src/factories/page';
-// import path from 'path';
-// import fs from 'fs';
-// import fsExtra, { writeJSON } from 'fs-extra';
+import path from 'path';
+import fs from 'fs';
 import { FormItemConfig, FormItem } from '../formItem';
 import { spawnSync } from 'child_process';
-
+import { changeConfig } from './changeConfig';
 /**
  * Input组件
  */
@@ -20,7 +19,7 @@ export default class KMSCloudComponent extends FormItem {
         this.packageName = `@ks/kms-${this.componentName.toLowerCase()}`;
         this.config = config;
         this.install();
-        // this.registerComponent();
+        this.registerComponent();
         // this.updateCloudModel();
     }
 
@@ -35,30 +34,17 @@ export default class KMSCloudComponent extends FormItem {
         );
     }
 
-    // /* 更新以来redux */
-    // async updateCloudModel() {
-    //     console.log('updateCloudModel');
-    //     console.log('process.cwd()', process.cwd());
-    //     const cloudModelPath = path.join(process.cwd(), 'src/default.json');
-    //     console.log('cloudModelPath', cloudModelPath);
-    //     const cloudModelJSON = await fsExtra.readJSONSync(cloudModelPath);
-    //     console.log('cloudModelJSON', cloudModelJSON);
-    //     cloudModelJSON[`${this.componentName}`] = `components/${this.packageName}/src/model`;
-    //     await writeJSON(cloudModelPath, cloudModelJSON);
-    // }
-
     /* 注册组件 */
-    // registerComponent = async () => {
-    //     console.log('registerComponent');
-    //     const registerPath = path.join(process.cwd(), 'src/default.js');
-    //     console.log('registerPath', registerPath);
-    //     const registerJSON = await fsExtra.readJSONSync(registerPath) || {};
-    //     const config = fs.readFileSync('/Users/m/Desktop/project/CMS/ks-pcweb-cms-pangu/src/default.js', { encoding: 'utf-8' });
-    //     console.log('config', config);
-    //     console.log('registerJSON', registerJSON);
-    //     registerJSON[`${this.componentName}`] = `Src/components/${this.packageName}/src/index`;
-    //     await writeJSON(registerPath, registerJSON);
-    // }
+    registerComponent = async () => {
+        const registerPath = path.join(process.cwd(), 'src/default.js');
+        const defaultConfig = fs.readFileSync(registerPath, { encoding: 'utf-8' });
+        const insertData = `Src/components/@ks/kms-condition/src/model`; // 暂时写死
+        // console.log('defaultConfig', defaultConfig);
+        let handleConfig = changeConfig(defaultConfig, insertData);
+        console.log('handleConfig', handleConfig);
+        // registerJSON[`${this.componentName}`] = `Src/components/${this.packageName}/src/index`;
+        // await writeJSON(registerPath, registerJSON);
+    }
 
     getDecoratorConfigCode() {
         return '{}';
---
+++ packages/ks-thanos-cli/src/factories/component/cloudComponent/changeConfig.ts
@@ -0,0 +1,106 @@
+import acorn from 'acorn';
+import escodegen from 'escodegen';
+
+const isObject = (obj: any) => {
+    return Object.prototype.toString.call(obj) === '[object Object]';
+};
+const isArray = (obj: any) => {
+    return Object.prototype.toString.call(obj) === '[object Array]';
+};
+
+interface childrenKeys {
+    type: string;
+    key: string;
+    value: string;
+}
+// ast
+const CHILDREN_KEYS: childrenKeys[] = [
+    { type: 'Program', key: '', value: 'body' },
+    { type: 'ExportNamedDeclaration', key: '', value: 'declaration' },
+    { type: 'VariableDeclaration', key: '', value: 'declarations' },
+    { type: 'VariableDeclarator', key: 'id', value: 'init' },
+    { type: 'ObjectExpression', key: '', value: 'properties' },
+    { type: 'Property', key: 'key', value: 'value' },
+    { type: 'ArrayExpression', key: '', value: 'elements' },
+    { type: 'ImportExpression', key: '', value: 'source' },
+    { type: 'Literal', key: '', value: '' },
+];
+
+// js转ast
+const parseTree = (config: any) => {
+    console.log('config', config);
+    let parseData = acorn.parse(config, {
+        ecmaVersion: 2020,
+        sourceType: 'module'
+    })
+    console.log('parseData', parseData);
+    return parseData;
+};
+// ast转js
+const escodegenTree = (parseTree: any) => {
+    return escodegen.generate(parseTree);
+};
+
+// 找到当前节点key
+const findTreeKey = (treeType: any): any => {
+    return (CHILDREN_KEYS.find(({ type }) => type === treeType) || {}).key;
+}
+// 找当前节点valueKey
+const findTreeValueKey = (treeType: string): any => {
+    return (CHILDREN_KEYS.find(({ type }) => type === treeType) || {}).value;
+}
+
+// 处理目标节点
+const handleFunc = (nodeValue: any, insertData: any) => {
+    let currTreeValue = nodeValue,
+        currTreeChildrenValue = currTreeValue[findTreeValueKey(currTreeValue.type)],
+        currTreeChildrenValueLength = currTreeChildrenValue.length,
+        endValue = currTreeChildrenValue[currTreeChildrenValueLength - 1],
+        hasCurrValue = currTreeChildrenValue.some((val: any) => `${val.source.value}` === `${insertData}`);
+    if (hasCurrValue) { return; };
+        let newValue = {
+            ...endValue,
+            start: endValue.end + 10,
+            end: endValue.end + 10 + insertData.length,
+            source: {
+                ...endValue.source,
+                start: endValue.end + 17,
+                end: endValue.end + 9 + insertData.length,
+                value: insertData,
+                raw: insertData
+            }
+        };
+        currTreeChildrenValue.push(newValue);
+}
+
+// 寻找目标节点
+const recursionTree = (tree: any, insertData?: any) => {
+    let treeType = tree.type,
+        treeKey = findTreeKey(treeType),
+        treeValueKey = findTreeValueKey(treeType),
+        treeValue = tree[treeValueKey];
+
+    if (treeKey && tree[treeKey].name === 'model') { // 目标节点特征
+        handleFunc(treeValue, insertData);
+    } else {
+        if (isArray(tree)) {
+            tree.forEach((tr: any) => {
+                recursionTree(tr)
+            })
+        } else if (isObject(tree)) {
+            if (treeValueKey) {
+                recursionTree(treeValue);
+            }
+        }
+    }
+}
+
+export const changeConfig = (config: any, insertData: any) => {
+    console.log('111');
+    let asTree = parseTree(config); // js转ast
+    console.log('asTree', asTree);
+    recursionTree(asTree, insertData); // 处理ast
+    let handleData = escodegenTree(asTree); // ast转js
+    console.log('handleData', handleData);
+    return handleData;
+}
