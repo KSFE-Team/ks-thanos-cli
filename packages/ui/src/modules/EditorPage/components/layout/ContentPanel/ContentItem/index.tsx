9d62e8e49247936f405a817d584ef9e20438b2ad
zhangyajun <zhangyajun@ksjgs.com>
2020-11-13 15:10:55 +0800

    style:组件库ui调整

--- packages/ui/src/modules/EditorPage/components/layout/ContentPanel/ContentItem/index.tsx
+++ packages/ui/src/modules/EditorPage/components/layout/ContentPanel/ContentItem/index.tsx
@@ -5,12 +5,12 @@ export default (props: any) => {
     const { data } = props;
     return (
         <div className="thanos-editor-block-item" data-name={data.componentName}>
-            <div className="thanos-editor-block-shortscreen">
+            {/* <div className="thanos-editor-block-shortscreen">
                 <img
                     style={{ maxWidth: '100%' }}
                     src={data.img || 'https://zos.alipayobjects.com/rmsportal/ndmJrWwkQloTtKg.jpg'}
                 />
-            </div>
+            </div> */}
             <div className="thanos-editor-block-title">{data.name}</div>
         </div>
     );
